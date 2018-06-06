c1340aaa3781b6f505bff7350f22214f5482ae3d
masaki.arizuka <masaki.arizuka@gmail.com>
2018-06-06 22:59:49 +0900

    Add function to return a next prime number

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+#[no_mangle]
+pub extern "C" fn get_prime(start: u32) -> u32 {
+    let mut i = start + 1;
+    loop {
+        if is_prime(i) {
+            return i;
+        }
+        i = i + 1;
+    }
+}
+
+fn is_prime(v: u32) -> bool {
+    match v {
+        1 => true,
+        2 => true,
+        _ => {
+            for i in 2..(v - 1) {
+                if v % i == 0 {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
